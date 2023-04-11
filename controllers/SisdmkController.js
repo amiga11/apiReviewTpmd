d17447d0febd8e2ab5dc8dd6a599562a41806cfe
Bima Putra <v171120011@bussan.co.id>
2023-04-11 10:04:56 +0700

    update

--- controllers/SisdmkController.js
+++ controllers/SisdmkController.js
@@ -2,196 +2,42 @@ import { kabkota, propinsi } from "../models/RegionalModel.js";
 import { data_pm, data_sisdmk, data_sisdmk_pekerjaan, trans_final } from "../models/SisdmkModel.js";
 
 
-// export const getListNakesbyCodeFaskes = async (req, res) => {
-//     trans_final.findOne({
-//         where: {
-//             kode_faskes: req.query.kodefaskes
-//         }
-//     }).then((results) => {
-//         const idFaskes = results.dataValues.id_faskes;
-//         data_sisdmk.findAll({
-//             attributes: ["NAMA"],
-//             where: {
-//                 id_faskes: idFaskes,
-
-//             },
-//             include: {
-//                 model: data_sisdmk_pekerjaan,
-//                 attributes: ["JENIS_SDMK", "SIP", "STR"]
-//             }
-//         }).then((resultsdata) => {
-//             res.status(200).send({
-//                 status: true,
-//                 message: "data found",
-//                 dataRS: results,
-//                 dataNakes: resultsdata,
-//             });
-//         }).catch((err) => {
-//             res.status(422).send({
-//                 status: false,
-//                 message: err,
-//             });
-//             return;
-//         })
-//     })
-// }
-
-
-///////////////////////////////////////
-
-// export const getListNakesbyCodeFaskes = async (req, res) => {
-//     trans_final.findOne({
-//         where: {
-//             kode_faskes: req.query.kodefaskes
-//         }
-//     }).then((results) => {
-//         const idFaskes = results.dataValues.id_faskes;
-//         data_pm.findOne({
-//             attributes: ["nama_pm"],
-//             where: {
-//                 id_faskes: idFaskes
-//             }
-//         }).then((resultsFasyankes) => {
-// data_sisdmk.findAll({
-//     attributes: ["NAMA"],
-//     where: {
-//         id_faskes: idFaskes,
-
-//     },
-//     include: {
-//         model: data_sisdmk_pekerjaan,
-//         attributes: ["JENIS_SDMK", "SIP", "STR"]
-//     }
-//             })
-//         }).then((resultsdata) => {
-//             res.status(200).send({
-//                 status: true,
-//                 message: "data found",
-//                 dataRS: resultsFasyankes.dataValues.nama_pm,
-//                 dataNakes: resultsdata,
-//             });
-//         }).catch((err) => {
-//             res.status(422).send({
-//                 status: false,
-//                 message: err,
-//             });
-//             return;
-//         })
-//     })
-// }
-
-
-
-// export const getListNakesbyCodeFaskes = async (req, res) => {
-//     trans_final.findOne({
-//         where: {
-//             kode_faskes: req.query.kodefaskes
-//         }
-//     }).then((results) => {
-//         const idFaskes = results.dataValues.id_faskes;
-//         data_pm.findOne({
-//             attributes: ["nama_pm"],
-//             where: {
-//                 id_faskes: idFaskes
-//             }
-//         }).then((resultsFasyankes) => {
-//             res.status(200).send({
-//                 status: true,
-//                 message: "data found",
-//                 kodeFasyankes: results.dataValues.kode_faskes_baru,
-//                 namaFasyankes: resultsFasyankes.dataValues.nama_pm,
-
-//             });
-//         }).catch((err) => {
-//             res.status(422).send({
-//                 status: false,
-//                 message: err,
-//             });
-//             return;
-//         })
-//     })
-// }
-
-
-
-export const getListNakesbyCodeFaskes = async (req, res) => {
+export const getListNakesbyKodeFaskes = async (req, res) => {
     trans_final.findOne({
         where: {
-            kode_faskes: req.query.kodefaskes
+            kode_faskes: req.params.kodefaskes
         }
     }).then((results) => {
-        const idFaskes = results.dataValues.id_faskes;
-        data_pm.findOne({
-            attributes: ["nama_pm"],
-            where: {
-                id_faskes: idFaskes
-            }
-        }).then((resultsFasyankes) => {
-            data_sisdmk.findAll({
-                attributes: [["NAMA", "nama"]],
+        if (results == null) {
+            res.status(200).send({
+                status: false,
+                message: "data not found",
+            });
+        } else {
+            const idFaskes = results.dataValues.id_faskes;
+            data_pm.findOne({
+                attributes: ["nama_pm"],
                 where: {
-                    id_faskes: idFaskes,
-                },
-                include: {
-                    model: data_sisdmk_pekerjaan, as: "detail_pekerjaan",
-                    attributes: [["JENIS_SDMK", "spesialisasi"], ["SIP", "sip"], ["STR", "str"]]
+                    id_faskes: idFaskes
                 }
-            }).then((resultsNakes) => {
-                res.status(200).send({
-                    status: true,
-                    message: "data found",
-                    data: {
-                        kodeFasyankes: results.dataValues.kode_faskes_baru,
-                        namaFasyankes: resultsFasyankes.dataValues.nama_pm,
-                        dataNakes: resultsNakes
-                    }
-                });
-            }).catch((err) => {
-                res.status(422).send({
-                    status: false,
-                    message: err,
-                });
-                return;
-            })
-        })
-    })
-}
-
-export const getDataFasyankes = async (req, res) => {
-    trans_final.findOne({
-        where: {
-            kode_faskes: req.query.kodefaskes
-        }
-    }).then((results) => {
-        const idFaskes = results.dataValues.id_faskes;
-        data_pm.findOne({
-            attributes: ["nama_pm", "alamat_faskes", "id_prov_pm", "id_kota_pm"],
-            where: {
-                id_faskes: idFaskes
-            }
-        }).then((resultsFasyankes) => {
-            const idprop = resultsFasyankes.dataValues.id_prov_pm;
-            propinsi.findOne({
-                attributes: ["nama_prop"],
-                where: {
-                    id_prop: idprop,
-                }
-            }).then((resultsprop) => {
-                const idkabkot = resultsFasyankes.dataValues.id_kota_pm
-                kabkota.findOne({
-                    attributes: ["nama_kota"],
+            }).then((resultsFasyankes) => {
+                data_sisdmk.findAll({
+                    attributes: [["NAMA", "nama"]],
                     where: {
-                        id_kota: idkabkot,
+                        id_faskes: idFaskes,
+                    },
+                    include: {
+                        model: data_sisdmk_pekerjaan, as: "detail_pekerjaan",
+                        attributes: [["JENIS_SDMK", "spesialisasi"], ["SIP", "sip"], ["STR", "str"]]
                     }
-                }).then((resultskabkot) => {
+                }).then((resultsNakes) => {
                     res.status(200).send({
                         status: true,
                         message: "data found",
                         data: {
+                            kodeFasyankes: results.dataValues.kode_faskes_baru,
                             namaFasyankes: resultsFasyankes.dataValues.nama_pm,
-                            propinsi: resultsprop.dataValues.nama_prop,
-                            kabKota: resultskabkot.dataValues.nama_kota,
-                            alamat: resultsFasyankes.dataValues.alamat_faskes,
+                            dataNakes: resultsNakes
                         }
                     });
                 }).catch((err) => {
@@ -202,16 +48,112 @@ export const getDataFasyankes = async (req, res) => {
                     return;
                 })
             })
-
-        })
+        }
     })
 }
 
+export const getDataFasyankesbyKodeFaskes = async (req, res) => {
+    trans_final.findOne({
+        where: {
+            kode_faskes: req.params.kodefaskes
+        }
+    }).then((results) => {
+        if (results == null) {
+            res.status(200).send({
+                status: false,
+                message: "data not found",
+            });
+        } else {
+            const idFaskes = results.dataValues.id_faskes;
+            data_pm.findOne({
+                attributes: ["nama_pm", "alamat_faskes", "id_prov_pm", "id_kota_pm"],
+                where: {
+                    id_faskes: idFaskes
+                }
+            }).then((resultsFasyankes) => {
+                const idprop = resultsFasyankes.dataValues.id_prov_pm;
+                propinsi.findOne({
+                    attributes: ["nama_prop"],
+                    where: {
+                        id_prop: idprop,
+                    }
+                }).then((resultsprop) => {
+                    const idkabkot = resultsFasyankes.dataValues.id_kota_pm
+                    kabkota.findOne({
+                        attributes: ["nama_kota"],
+                        where: {
+                            id_kota: idkabkot,
+                        }
+                    }).then((resultskabkot) => {
+                        res.status(200).send({
+                            status: true,
+                            message: "data found",
+                            data: {
+                                namaFasyankes: resultsFasyankes.dataValues.nama_pm,
+                                propinsi: resultsprop.dataValues.nama_prop,
+                                kabKota: resultskabkot.dataValues.nama_kota,
+                                alamat: resultsFasyankes.dataValues.alamat_faskes,
+                            }
+                        });
+                    }).catch((err) => {
+                        res.status(422).send({
+                            status: false,
+                            message: err,
+                        });
+                        return;
+                    })
+                })
+            })
+        }
+    })
+}
 
+// export const getListFasyankesbyKodeFaskes = async (req, res) => {
+//     trans_final.findOne({
+//         where: {
+//             kode_faskes: req.params.kodefaskes
+//         }
+//     }).then((results) => {
+//         const idFaskes = results.dataValues.id_faskes;
+//         data_pm.findOne({
+//             attributes: ["nama_pm", "alamat_faskes", "id_prov_pm", "id_kota_pm"],
+//             where: {
+//                 id_faskes: idFaskes
+//             }
+//         }).then((resultsFasyankes) => {
+//             const idprop = resultsFasyankes.dataValues.id_prov_pm;
+//             propinsi.findOne({
+//                 attributes: ["nama_prop"],
+//                 where: {
+//                     id_prop: idprop,
+//                 }
+//             }).then((resultsprop) => {
+//                 const idkabkot = resultsFasyankes.dataValues.id_kota_pm
+//                 kabkota.findOne({
+//                     attributes: ["nama_kota"],
+//                     where: {
+//                         id_kota: idkabkot,
+//                     }
+//                 }).then((resultskabkot) => {
+//                     res.status(200).send({
+//                         status: true,
+//                         message: "data found",
+//                         data: {
+//                             namaFasyankes: resultsFasyankes.dataValues.nama_pm,
+//                             propinsi: resultsprop.dataValues.nama_prop,
+//                             kabKota: resultskabkot.dataValues.nama_kota,
+//                             alamat: resultsFasyankes.dataValues.alamat_faskes,
+//                         }
+//                     });
+//                 }).catch((err) => {
+//                     res.status(422).send({
+//                         status: false,
+//                         message: err,
+//                     });
+//                     return;
+//                 })
+//             })
 
-// const namakabkota = kabkota.findOne({
-//     attributes: ["nama_kota"],
-//     where: {
-//         id_prop: idkabkot,
-//     }
-// })
+//         })
+//     })
+// }
--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,17 @@
 import express from "express";
 
 import { insertReview } from "../controllers/ReviewController.js";
-import { getDataFasyankes, getListNakesbyCodeFaskes } from "../controllers/SisdmkController.js";
+import { getDataFasyankesbyKodeFaskes, getListNakesbyKodeFaskes } from "../controllers/SisdmkController.js";
 import { changePassword, getDataUser, insertDataUser, login, logout } from "../controllers/UsersController.js";
 import { verifyToken } from "../middleware/VerifyToken.js";
+
 
 const router = express.Router();
 
 router.post("/reviewtpmd/v1/login", login);
 router.post("/reviewtpmd/v1/review/submit", verifyToken, insertReview);
 
-router.get("/reviewtpmd/v1/nakes", verifyToken, getListNakesbyCodeFaskes);
-router.get("/reviewtpmd/v1/fasyankes", verifyToken, getDataFasyankes);
+router.get("/reviewtpmd/v1/nakes/:kodefaskes", verifyToken, getListNakesbyKodeFaskes);
+router.get("/reviewtpmd/v1/fasyankes/:kodefaskes", verifyToken, getDataFasyankesbyKodeFaskes);
+
 export default router;
